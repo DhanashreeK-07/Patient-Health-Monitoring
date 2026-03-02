d9295d16af2e2a54db35f10a48c468dda280bb54
Dhanashree Kadam <138194783+DhanashreeK-07@users.noreply.github.com>
2026-03-02 18:17:58 +0530

    Delete schema.py

--- schema.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql.types import *
-
-schema = StructType([
-    StructField("patient_id", StringType()),
-    StructField("timestamp", StringType()),
-    StructField("heart_rate", IntegerType()),
-    StructField("oxygen_level", IntegerType()),
-    StructField("temperature", DoubleType())
-])
